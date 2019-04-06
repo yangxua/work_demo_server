bbc51c117bd30639fdbe77f4e3a9b7b8dc466664
allanyang <allanyang@lexinfintech.com>
2019-04-06 13:20:24 +0800

    可配置

---
+++ work-design-pattern-server/src/main/java/utils/XMLUtil.java
@@ -0,0 +1,37 @@
+package utils;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+/**
+ * @Auther: allanyang
+ * @Date: 2019/4/6 13:18
+ * @Description:
+ */
+public class XMLUtil {
+
+    public static String getChartType() {
+        try {
+            //创建文档对象
+            DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = dFactory.newDocumentBuilder();
+            Document doc;
+            doc = builder.parse(new File("E:\\Git\\work-design-pattern-server\\src\\main\\resources\\config.xml"));
+
+            //获取包含图表类型的文本节点
+            NodeList nl = doc.getElementsByTagName("chartType");
+            Node classNode = nl.item(0).getFirstChild();
+            String chartType = classNode.getNodeValue().trim();
+            return chartType;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
