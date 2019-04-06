b26cf0fa418c9327da2c1110f547134d766473b6
allanyang <allanyang@lexinfintech.com>
2019-04-06 14:58:05 +0800

    修改XMLUtil为getBean

--- work-design-pattern-server/src/main/java/utils/XMLUtil.java
+++ work-design-pattern-server/src/main/java/utils/XMLUtil.java
@@ -15,7 +15,7 @@ import java.io.File;
  */
 public class XMLUtil {
 
-    public static String getChartType() {
+    public static Object getBean() {
         try {
             //创建文档对象
             DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
@@ -23,15 +23,20 @@ public class XMLUtil {
             Document doc;
             doc = builder.parse(new File("E:\\Git\\work-design-pattern-server\\src\\main\\resources\\config.xml"));
 
-            //获取包含图表类型的文本节点
-            NodeList nl = doc.getElementsByTagName("chartType");
-            Node classNode = nl.item(0).getFirstChild();
-            String chartType = classNode.getNodeValue().trim();
-            return chartType;
+            //获取包含类名的文本节点
+            NodeList nl = doc.getElementsByTagName("className");
+            Node classNode=nl.item(0).getFirstChild();
+            String cName=classNode.getNodeValue();
+
+            //通过类名生成实例对象并将其返回
+            Class c=Class.forName(cName);
+            Object obj=c.newInstance();
+            return obj;
         }
         catch(Exception e) {
             e.printStackTrace();
             return null;
         }
     }
+
 }
